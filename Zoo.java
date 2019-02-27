b24083bca57d384fbdd5a0d94af3506f66382c3e
Jiyeon Lee <jiyeonlee@Jiyeons-MacBook.local>
2019-02-27 17:09:26 -0600

    first commit

--- Zoo.java
+++ Zoo.java
@@ -1,34 +1,37 @@
-import java.util.*;
-import java.util.Collections;
+import java.util.TreeMap;
+import java.util.Scanner;
+import java.util.Map;
 
 public class Zoo {
-   public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
-      input.useLocale(Locale.US);
+   public static void main (String[] args) {
+      Scanner in = new Scanner(System.in);
+      int n = Integer.parseInt(in.nextLine());
+      int aniC = 0;
       
-      int n = Integer.parseInt(input.nextLine());
-      int listC = 0;
-                        
-      while(n != 0) {
-         TreeMap<String, Integer> zooList = new TreeMap<>();
-      
-         for(int i = 0; i < n; i++) {
-            String animalName = last(input.nextLine().split(" ")).toLowerCase();
-            zooList.putIfAbsent(animalName, 0);
-            zooList.put(animalName, zooList.get(animalName) + 1);
+      while (n != 0) {
+         TreeMap<String, Integer> animals = new TreeMap<String, Integer>();
+         
+         for (int i = 0; i < n; i++) {
+            String inp = in.nextLine();
+            String[] inpSplit = inp.split(" ");
+            String animal = inpSplit[inpSplit.length - 1].toLowerCase();
+            
+            if(animals.containsKey(animal)) {
+               int oldValue = animals.get(animal);
+               animals.put(animal, oldValue + 1);
+            }
+            else {
+               animals.put(animal, 1);
+            }
          }
          
-         System.out.println("List " + ++listC + ":");
+         System.out.println("List " + ++aniC + ":");
          
-         for(Map.Entry<String, Integer> eachEn : zooList.entrySet()) {
-            System.out.printf("%s | %d\n", eachEn.getKey(), eachEn.getValue());
-         } 
-         
-         n = Integer.parseInt(input.nextLine());     
-      }          
-   }
-   
-   private static String last(String[] arr) {
-      return arr[arr.length - 1];
+         for (String animalType: animals.keySet()) {
+            System.out.println(animalType + " | " + animals.get(animalType));
+         }
+           
+         n = Integer.parseInt(in.nextLine());
+      }
    }
 }
